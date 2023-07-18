dafff2577453dcaa7e1458aaf544029bf4df2da9
Deku <zachdsamson@gmail.com>
2023-07-17 18:25:07 -0700

    wrote a range and sum function to create and sum an array with a start and end point

--- Chapter4/script.js
+++ Chapter4/script.js
@@ -1,1 +1,22 @@
-console.log('hello')
+// add optional step value to RANGE function
+let range = function(start, end){
+    let counter = end - start;
+    let outputArray = [];
+    for(i = 0; i < counter + 1; i++ ){
+        let currentNumber = start + i;
+        outputArray.push(currentNumber);
+    }
+    return outputArray;
+}
+
+console.log(range(1, 10));
+
+let sum = function(array){
+    let numberStorage = 0;
+    for(let element of array){
+        numberStorage = numberStorage + element;
+    }
+    return(numberStorage);
+}
+
+console.log(sum(range(1,10)));
