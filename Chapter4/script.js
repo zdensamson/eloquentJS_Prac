7c83ca3e1ce235c2c25cfb6c7dab029d5427fc41
Deku <zachdsamson@gmail.com>
2023-07-18 18:55:43 -0700

    added an optional STEP argument to the range function allowing a user to alter the default count of 1 if they choose

--- Chapter4/script.js
+++ Chapter4/script.js
@@ -1,15 +1,44 @@
 // add optional step value to RANGE function
-let range = function(start, end){
+let range = function(start, end, step){
     let counter = end - start;
     let outputArray = [];
-    for(i = 0; i < counter + 1; i++ ){
-        let currentNumber = start + i;
-        outputArray.push(currentNumber);
+
+    if(step == null){
+        for(i = 0; i < counter + 1; i++ ){
+            let currentNumber = start + i;
+            outputArray.push(currentNumber);
+        }
+        return outputArray;
+    }else if(step > 0){
+        for(i = 0; i < end ; i = i + step ){
+            let currentNumber = start + i;
+            if(currentNumber < end || currentNumber == end){
+                outputArray.push(currentNumber);
+            }else{
+                return outputArray;
+            }  
+        }
+        return outputArray;
+    }else{
+        for(i = 0; -i < start; i = i + step ){
+            let currentNumber = start + i;
+            if(currentNumber > end || currentNumber == end){
+                outputArray.push(currentNumber);
+            }else{
+                return outputArray;
+            }  
+        }
+        return outputArray;
     }
-    return outputArray;
+
 }
 
-console.log(range(1, 10));
+console.log(range(5, 2, -1));
+console.log(range(10, 0, -3));
+console.log(range(10, 0, -11));
+console.log(range(1, 10, 2));
+console.log(range(1, 10, 15));
+
 
 let sum = function(array){
     let numberStorage = 0;
@@ -19,4 +48,4 @@ let sum = function(array){
     return(numberStorage);
 }
 
-console.log(sum(range(1,10)));
+// console.log(sum(range(1,10)));
