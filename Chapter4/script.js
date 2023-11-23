45a31595bbf865334217ed7807d993a12e10a187
Deku <zachdsamson@gmail.com>
2023-11-23 11:44:19 -0700

    moving on

--- Chapter4/script.js
+++ Chapter4/script.js
@@ -57,10 +57,45 @@ let reverseArrayInPlace = function(array){
         temp = array[i];
         array[i] = array[array.length - 1 - i];
         array[array.length - 1 - i] = temp;
-  
+
+        // array = [1,2,3] array.length = 3 array.length/2 = 1.5
+            // i = 0
+                // temp = array[0] => 1
+                // ari = array[array.length - 1 - i]
+                    // => ari = array[3 - 1 - 0] ==> ari = array[2] ==> ari =  3
+                    // ARRAY[0] = 3
+                // arili = array[array.length - 1 - i] = temp
+                    // => arili = array[3 - 1 - 0] = temp 
+                    // => arili = array[2] => arili = 3 ==> ARRAY[3] = temp
+                    // ARRAY[3] = 1
+            
+            // i = 2 --> 2 > 1.5 STOP
+
+
+        // array = [1,2,3,4] | array.length = 4 | array.length/2 = 2
+            // i = 0
+                // temp = array[0] 
+                    // TEMP = 1
+                // array[0] =  array[4 - 1 -0] ==> [*,2,3,4] = 4
+                // array[3] = temp ==> [1,2,3,*] =  1
+                
+            // i = 1-- array = [4,2,3,1]
+                // temp = array[1]
+                    // temp = 2
+                // array[1] = array[4-1-1] ==> [4,*,3,1] = array[2] ==> [4,*,3,1] = 3
     }
     return array;
 };
+
+let arr1 = [1,2,3,4];
+t = arr1[0];
+arr1[0] = arr1[arr1.length - 1];
+console.log(t);
+console.log(arr1[0]);
+
+
+// console.log(reverseArrayInPlace([1,2,3]));
+
 
 let arrayToList = function(array){
     console.log(array.length);
@@ -122,6 +157,18 @@ let sum2 = function(numArray){
     return numberHolder;
 }
 
-console.log(range2(1,10));
+let reverseArray2 = function(array){
+    let newArray = [];
+    for (i = 0; i < array.length ; i++){
+        newArray.push(array[array.length - i -1]);
+    }
+    return newArray;
+}
 
-console.log(sum(range(1,10)));
+// arrEx = [1,2,3]
+
+// console.log(arrEx.length)
+let reverseArrayInPlace2 = function(array){
+
+
+}
