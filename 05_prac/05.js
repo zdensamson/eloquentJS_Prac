6e9361cc410c50e985c7d754c00100bce3586877
Deku <zachdsamson@gmail.com>
2023-11-24 21:08:35 -0700

    additional higher order func

--- 05_prac/05.js
+++ 05_prac/05.js
@@ -16,10 +16,19 @@ function noisy(f) {
 
 // noisy(Math.min)(3,2,1);
 
+function repeat(n, action){
+   for(let i = 0; i < n; i++){
+      action(i);
+   }
+}
+
+
 function unless(test, then) {
    if (!test) then();
 }
 
 repeat(3, n => {
-   
-})
+   unless(n % 2 == 1, () =>{
+      console.log(n ,"is even")
+   });
+});
